8a11e7ab1046f2e2a21c19f041a10ac63a544e65
Ayush Garg <ayushkumargarg1997@gmail.com>
2025-08-24 17:36:28 +0530

    clean_up

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-CLAUDE_API_KEY = os.getenv("CLAUDE_API_KEY")
-SENDER_EMAIL = os.getenv("SENDER_EMAIL")
-SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
